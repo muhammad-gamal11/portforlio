b7edfe416410a36ed776b82da71e47c054aa2079
Muhammad Gamal <gamal9978@gmail.com>
2024-03-01 23:09:00 +0200

    changed how the skills look on mobile screen

--- src/components/about/Skills.js
+++ src/components/about/Skills.js
@@ -5,37 +5,6 @@ import ResumeTitle from "./ResumeTitle";
 const Skills = () => {
   return (
     <div className="w-full grid grid-cols-9 gap-10 lgl:gap-0 px-6">
-      {/* =============== Languages Start here =================== */}
-      <div className="col-span-9 md:col-span-4">
-        <ResumeTitle title="Languages" icon={<FaFlag />} />
-
-        <div className="py-4">
-          <div className="py-4 border-b-[1px] border-zinc-800">
-            <p className="text-base text-textColor -mb-1.5">Arabic</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
-              <span className="w-[100%] h-full absolute top-0 left-0 bg-designColor"></span>
-            </span>
-          </div>
-          <div className="py-4 border-b-[1px] border-zinc-800">
-            <p className="text-base text-textColor -mb-1.5">English</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
-              <span className="w-[100%] h-full absolute top-0 left-0 bg-designColor"></span>
-            </span>
-          </div>
-
-          <div className="py-4 border-b-[1px] border-zinc-800">
-            <p className="text-base text-textColor -mb-1.5">Spanish</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
-              <span className="w-[80%] h-full absolute top-0 left-0 bg-designColor"></span>
-            </span>
-          </div>
-        </div>
-      </div>
-      {/* =============== Languages End here =================== */}
-
-      <div className="w-full h-full hidden lgl:flex justify-center items-center">
-        <span className="w-[1px] h-full bg-zinc-800 inline-flex"></span>
-      </div>
       {/* =============== Skills Start here =================== */}
 
       <div className="col-span-9 md:col-span-4">
@@ -101,6 +70,38 @@ const Skills = () => {
         </div>
       </div>
       {/* =============== Skills end here =================== */}
+
+      <div className="w-full h-full hidden lgl:flex justify-center items-center">
+        <span className="w-[1px] h-full bg-zinc-800 inline-flex"></span>
+      </div>
+
+      {/* =============== Languages Start here =================== */}
+      <div className="col-span-9 md:col-span-4">
+        <ResumeTitle title="Languages" icon={<FaFlag />} />
+
+        <div className="py-4">
+          <div className="py-4 border-b-[1px] border-zinc-800">
+            <p className="text-base text-textColor -mb-1.5">Arabic</p>
+            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+              <span className="w-[100%] h-full absolute top-0 left-0 bg-designColor"></span>
+            </span>
+          </div>
+          <div className="py-4 border-b-[1px] border-zinc-800">
+            <p className="text-base text-textColor -mb-1.5">English</p>
+            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+              <span className="w-[100%] h-full absolute top-0 left-0 bg-designColor"></span>
+            </span>
+          </div>
+
+          <div className="py-4 border-b-[1px] border-zinc-800">
+            <p className="text-base text-textColor -mb-1.5">Spanish</p>
+            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+              <span className="w-[80%] h-full absolute top-0 left-0 bg-designColor"></span>
+            </span>
+          </div>
+        </div>
+      </div>
+      {/* =============== Languages End here =================== */}
     </div>
   );
 };
